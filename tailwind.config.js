311fd1d6f9c5f7f973be6cd780a34d9fdf71e130
Parvez-oos <mmd582024@gmail.com>
2026-04-16 19:43:21 +0600

    setup React project with Vite and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#2C5545', // KeenKeeper Dark Green
+      }
+    },
+  },
+  plugins: [],
+}
